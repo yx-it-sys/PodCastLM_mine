40ddd2e87acd5375dd0a36a671dd587e040d7150
yoyzhang <alexuzhang@gmail.com>
2024-10-17 17:37:49 +0800

    feat: add logo

--- frontend/src/lib/constant.ts
+++ frontend/src/lib/constant.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8000/api/v1/chat"
-export const HOST_URL = "http://localhost:8000"
+export const BASE_URL = "https://zhang-xxx-podcastlm-backend.hf.space/api/v1/chat"
+export const HOST_URL = "https://zhang-xxx-podcastlm-backend.hf.space"
